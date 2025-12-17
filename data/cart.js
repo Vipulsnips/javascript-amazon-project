472f7ec472639f1b4182572c83cc312343ae5b3c
Vipulsnips <vipulrawat137@gmail.com>
2025-12-17 12:16:09 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart=[
+export let cart=JSON.parse(localStorage.getItem('cart')) || [
 {
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity:2
@@ -6,6 +6,10 @@ export let cart=[
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:2
 }];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addtocart(productId){
 let matchingItem;
   const  quantity= Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
@@ -23,6 +27,7 @@ let matchingItem;
       quantity:quantity
     })
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -31,4 +36,5 @@ export function removeFromCart(productId){
     if(item.productId !== productId) newCart.push(item);
   })
   cart=newCart;
+  saveToStorage();
 }
